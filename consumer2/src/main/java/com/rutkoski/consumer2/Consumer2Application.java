79747d385fe4d1004a7d3cdd2864af29310c25b6
PauloRutkoski <paulo.rtkoski@gmail.com>
2021-12-15 23:22:44 -0300

    Second consumer

--- consumer2/src/main/java/com/rutkoski/consumer2/Consumer2Application.java
+++ consumer2/src/main/java/com/rutkoski/consumer2/Consumer2Application.java
@@ -1,23 +1,18 @@
-package com.rutkoski.consumer;
+package com.rutkoski.consumer2;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
-@EnableKafka
-public class ConsumerApplication {
+public class Consumer2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
+		SpringApplication.run(Consumer2Application.class, args);
 	}
 
-	@KafkaListener(id = "my_topic_tests", topics = "${topics.test}")
+	@KafkaListener(id = "my_topic_tests_2", topics = "${topics.test}")
 	public void testsTopicListener(Message msg) {
 		System.out.println("============================");
 		System.out.println("FROM: " + msg.getName());
---
+++ consumer2/src/main/java/com/rutkoski/consumer2/Message.java
@@ -0,0 +1,24 @@
+package com.rutkoski.consumer2;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private String name;
+    private String text;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
